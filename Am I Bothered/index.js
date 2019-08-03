8c4fd0a0baed36b9a87b91355a3597da0489285c
Luke <48770392+badgersmack@users.noreply.github.com>
2019-08-03 13:41:22 +0100

    Added files WIP
    Added files WIP

---
+++ Am I Bothered/index.js
@@ -0,0 +1,17 @@
+
+//bothered calc takes a percentage ands assigns a value using if statments
+//need to attach this to the class in index.html
+var bothered = Math.floor(Math.random() *100);
+
+document.getElementsByType("button")[i].addEventListener("click", function document.getElementByID ("result") {
+if (bothered === 100) {
+  innerHTML"TEXT"
+}
+if (bothered > 60 && bothered <= 99) {
+  //text
+}
+//other things here
+if (bothered < 10 && bothered > 60) {
+  //not bothered
+}
+}
