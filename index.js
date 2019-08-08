ab0dc2da74e1c6cb8df05a5d1ac3a483bce775fe
Luke <48770392+badgersmack@users.noreply.github.com>
2019-08-08 20:19:09 +0100

    Fixed javaScript

---
+++ index.js
@@ -0,0 +1,23 @@
+
+//bothered calc takes a percentage ands assigns a value using if statements
+//need to attach this to the class in index.html
+//var bothered = Math.floor(Math.random() *100);
+
+document.getElementById("butt").addEventListener("click", changeThingy);
+
+function changeThingy() {
+console.log("what");
+  document.getElementById("result").innerHTML ="proper bothered mate";
+}
+// {//
+//if (bothered === 100) {
+//  innerHTML"TEXT"
+//}
+//if (bothered > 60 && bothered <= 99) {
+
+//}
+//other things here
+//if (bothered < 10 && bothered > 60) {
+  //not bothered
+//}
+//}
